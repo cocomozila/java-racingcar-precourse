8e2986185530dde8f259abca716d5840f720095e
cocomozila <joseyun94@naver.com>
2022-11-26 22:17:55 +0900

    feat(inputView): 이름 입력값을 검증하는 기능

--- src/main/java/racingcar/input/InputView.java
+++ src/main/java/racingcar/input/InputView.java
@@ -2,14 +2,36 @@ package racingcar.input;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputView {
 
-    public List inputNames() {
+    public List<String> readCarNames() {
         String input = Console.readLine();
+        checkEmpty(input);
         String[] names = input.split(",");
-        return null;
+        validateCarNames(names);
+        return new ArrayList<>(Arrays.asList(names));
+    }
+
+    private void checkEmpty(String input) {
+        if (input.equals("")) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 필요합니다.");
+        }
+    }
+
+    private void validateCarNames(String[] names) {
+        for (String name : names) {
+            validateCarNameSize(name);
+        }
+    }
+
+    private void validateCarNameSize(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 이름은 5글자 이하만 가능합니다.");
+        }
     }
 
 }
