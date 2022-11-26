8dc8b3d951957a498b4548644fbb21aafed16f16
cocomozila <joseyun94@naver.com>
2022-11-26 21:40:51 +0900

    feat(inputView): 이름 입력 기능 추가

---
+++ src/main/java/racingcar/input/InputView.java
@@ -0,0 +1,15 @@
+package racingcar.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+
+    public List inputNames() {
+        String input = Console.readLine();
+        String[] names = input.split(",");
+        return null;
+    }
+
+}
