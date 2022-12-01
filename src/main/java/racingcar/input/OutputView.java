73f9c302faab54d0e200147cd90d0f0ed4818c28
cocomozila <joseyun94@naver.com>
2022-12-01 18:57:41 +0900

    feat(OutputView): 기본 메세지 출력 기능 추가

---
+++ src/main/java/racingcar/input/OutputView.java
@@ -0,0 +1,16 @@
+package racingcar.input;
+
+public class OutputView {
+
+    public void displayInputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void displayInputAttempt() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void displayExecutionResult() {
+        System.out.println("\n실행 결과");
+    }
+}
